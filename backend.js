68d4ba528c417bf544da15c344481245da559aa0
Akshat Pattiwar <akshatpattiwar512@gmail.com>
2022-10-05 10:37:42 +0530

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,11 @@
+// const body = document.getElementsByTagName('body');
+// const myCustomElement = document.createElement('my-custom-element');
+// body.appendChild(myCustomElement);
+// const collection = document.getElementsByTagName("body");
+// for (let i = 0; i < collection.length; i++) {
+//   collection[i].style.backgroundColor = "red";
+//   windows.alert(collection[i]);
+// }
+<google-map fit-to-markers api-key="AIzaSyD3E1D9b-Z7ekrT3tbhl_dy8DCXuIuDDRc">
+    <google-map-marker latitude="37.78" longitude="-122.4" draggable="true"></google-map-marker>
+</google-map>
